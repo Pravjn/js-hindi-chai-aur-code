80a606453c98ee13b2a539735725f9874693ec4f
Praveen Tiwari <125129631+Pravjn@users.noreply.github.com>
2025-06-09 11:47:57 +0000

    Modified

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -13,8 +13,8 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "Null"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => False
@@ -23,7 +23,53 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ******************************  Operations       *************************************************     
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+///console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello "
+let str2 = "Praveen "
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"  + 2 + 2);
+// console.log(2 + 2 + "1");
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+/* let x = 3;                   
+const y = x++;          => postfix increment operator x++
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;             => Prefix Increment operator ++x
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4" */
 
 
