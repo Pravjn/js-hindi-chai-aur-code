70f2ee6ae8c379bcda24c57e4448d013d8218092
Praveen Tiwari <125129631+Pravjn@users.noreply.github.com>
2025-06-09 07:01:10 +0000

    continued

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 2204
+let accountEmail ="tiwaripraveen@gmail.com"
+var accountPassword ="Praben@131" //prefer nt to use var becoz of issue in block code and functional code
+accountCity = "Solhapur"
+
+//accountId = 2 Not allowed
+
+accountEmail="praveen.com"
+accountPassword="cwapm2"
+accountCity="mumbai"
+
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ 01_basic/02_Data Types.js
@@ -0,0 +1,19 @@
+"use  strict"; // treat all JS as new version
+
+// alert( 3+ 3 ) we are using node.js not the browser
+
+console.log( 3 + 3);
+
+console.log("Praveen");
+// 2 to the power of 53
+// bignit
+// string => ""
+// boolean => true /false
+// null =>standalone value
+// undefined
+// symbol => unique
+
+
+//object
+
+console.log(typeof "praveen");
