74bee468189bdef39fd206ca29d3995466b596ea
Praveen Tiwari <125129631+Pravjn@users.noreply.github.com>
2025-06-09 12:18:53 +0000

    new file

---
+++ 01_basic/dataTypes-summary.js
@@ -0,0 +1,10 @@
+// # Primitive
+
+// 7 Types : string, Number, Boolean, null, undefined, Symbol(unique), BigInt   => call by value
+
+const score = 100
+
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
