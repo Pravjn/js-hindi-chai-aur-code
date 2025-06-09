942f76aead99303447637af0623f59a2b3b2ab42
Praveen Tiwari <125129631+Pravjn@users.noreply.github.com>
2025-06-09 12:09:45 +0000

    changes

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = 33
 
-//console.log(typeof score) ;
-//console.log(typeof (score)) ;
+console.log(typeof score) ;
+console.log(typeof (score)) ;
 
 let valueInNumber = Number(score)
-//console.log( typeof valueInNumber);
-//console.log(valueInNumber);
+console.log( typeof valueInNumber);
+console.log(valueInNumber);
 
 // "33" => 33
 //"33abc" => NaN
@@ -13,8 +13,8 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "Null"
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => False
@@ -23,34 +23,34 @@ let isLoggedIn = "Null"
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-//console.log(stringNumber);
-//console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // ******************************  Operations       *************************************************     
 let value = 3
 let negvalue = -value
-//console.log(negvalue)
+console.log(negvalue)
 
-//console.log(2+2);
-//console.log(2-2);
-//console.log(2*2);
-///console.log(2**2);
-//console.log(2/3);
-//console.log(2%3);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
 
 let str1 = "hello "
 let str2 = "Praveen "
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1"  + 2 + 2);
-// console.log(2 + 2 + "1");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"  + 2 + 2);
+console.log(2 + 2 + "1");
 
-//console.log(+true);
-//console.log(+"");
+console.log(+true);
+console.log(+"");
 
 let num1, num2, num3
 
