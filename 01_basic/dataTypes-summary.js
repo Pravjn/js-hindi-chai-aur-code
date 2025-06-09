585007e2eed00c8f437e2a4f594400c12a1de43a
Praveen Tiwari <125129631+Pravjn@users.noreply.github.com>
2025-06-09 14:14:09 +0000

    update

--- 01_basic/dataTypes-summary.js
+++ 01_basic/dataTypes-summary.js
@@ -3,8 +3,34 @@
 // 7 Types : string, Number, Boolean, null, undefined, Symbol(unique), BigInt   => call by value
 
 const score = 100
+const scoreValue = 100.34
 
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id === anotherId);
+
+//const bigNumber = 3456789n
 
 // Reference (Non-Primitive)
 
 // Array, Objects, Functions
+
+const heros = ("shaktiman", "Hanuman", "batman");
+let myObj = {
+    name : "Praveen",
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("HELLO WORLD");
+    
+}
+
+console.log(typeof heros);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
